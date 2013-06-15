222f60855362aeeb90e318772d864f795d6b449b
Jess Balint <jbalint@gmail.com>
2013-06-15 11:25:36 -0500

    generalize email rule processing

--- configs/imapfilter/oracle.lua
+++ configs/imapfilter/oracle.lua
@@ -1,10 +1,10 @@
+-- -*- indent-tabs-mode: nil -*-
 -- https://github.com/lefcha/imapfilter/blob/master/samples/config.lua
 ---------------
 --  Options  --
 ---------------
 options.timeout = 120
 options.subscribe = true
-
 
 ----------------
 --  Accounts  --
@@ -19,30 +19,50 @@ oracle = IMAP {
 -----------------
 --  Mailboxes  --
 -----------------
+
+imap_account = oracle
+inbox = imap_account["INBOX"]
+
+personal = {destination="followup",
+            sent_to={"jess.balint@oracle.com"},
+            other_rules={Mailbox.is_flagged}}
+connectors = {destination="connectors",
+              sent_to={"mysql-connectors_ww@oracle.com",
+                       "mysql-connectors_ww_grp@oracle.com",
+                       "mysql-connectors-java_grp@oracle.com"}}
+dev_bugs = {destination="dev-bugs",
+            sent_to={"mysql_dev-bugs_ww@oracle.com"}}
+dev_private = {destination="dev-private",
+               sent_to={"mysql_dev-private_ww_grp@oracle.com",
+                        "MYSQL_DEV-PRIVATE_WW@oracle.com"}}
+dev_commits = {destination="dev-commits",
+               sent_to={"mysql_commits_ww_grp@oracle.com",
+                        "mysql_commits_ww@oracle.com"}}
+fabric = {destination="fabric",
+          sent_to={"mysql_dev_ham_ww_grp@oracle.com"}}
+
 -- in order of priority
+--rules = {personal, connectors, dev_bugs, dev_private, dev_commits, fabric}
+rules = {personal, connectors, dev_bugs, dev_private, dev_commits, fabric}
 
-inbox = oracle["INBOX"]
+for i, rule in ipairs(rules) do
+   local msgs = {}
 
-personal_msgs = inbox:contain_to("jess.balint@oracle.com") +
-	      inbox:contain_cc("jess.balint@oracle.com")
-personal_msgs:move_messages(oracle["followup"])
+   -- handle sent_to rules
+   if not rule.sent_to then
+      rule.sent_to = {}
+   end
+   for i, addr in ipairs(rule.sent_to) do
+      msgs = msgs + inbox:contain_to(addr) + inbox:contain_cc(addr)
+   end
 
-connectors_msgs = inbox:contain_to("mysql-connectors_ww@oracle.com") +
-		inbox:contain_cc("mysql-connectors_ww@oracle.com") +
-		inbox:contain_to("mysql-connectors_ww_grp@oracle.com") +
-		inbox:contain_cc("mysql-connectors_ww_grp@oracle.com")
-connectors_msgs:move_messages(oracle["connectors"])
-
-dev_bugs_msgs = inbox:contain_to("mysql_dev-bugs_ww@oracle.com") +
-	      inbox:contain_cc("mysql_dev-bugs_ww@oracle.com")
-dev_bugs_msgs:move_messages(oracle["dev-bugs"])
-
-dev_priv_msgs = inbox:contain_to("asd@oracle.com")
-dev_priv_msgs:move_messages(oracle["dev-private"])
-
-dev_commits_msgs = inbox:contain_to("mysql_commits_ww_grp@oracle.com") +
-		 inbox:contain_cc("mysql_commits_ww_grp@oracle.com") +
-		 inbox:contain_to("mysql_commits_ww@oracle.com") +
-		 inbox:contain_cc("mysql_commits_ww@oracle.com")
-dev_commits_msgs:move_messages(oracle["dev-commits"])
+   -- handle other_rules, pass mailbox directly, should return messages
+   if not rule.other_rules then
+      rule.other_rules = {}
+   end
+   for i, sub_rule in ipairs(rule.other_rules) do
+      msgs = msgs + sub_rule(inbox)
+   end
+   msgs:move_messages(imap_account[rule.destination])
+end
 
